222926afeca977da70707edb31f88b2dfbdfb1c3
Syed Tauhid Ahmed <stahmed001@gmail.com>
2018-03-22 14:35:53 +0600

    Phase 1: Bootstrap ngx and routing added to the project.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app.routing';
 
 
 @NgModule({
