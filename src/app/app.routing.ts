0f2c03456d797e5f37a7ec3d9d47aa4ceeb21ee2
Syed Tauhid Ahmed <stahmed001@gmail.com>
2018-03-22 16:05:59 +0600

    Phase 1: Basic layout of app component designed.

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-const routes: Routes = [];
+const routes: Routes = [
+  // otherwise redirect to home
+  // { path: "movies", component: LoginComponent },
+  // { path: "admin", component: AdminpanelComponent },
+  { path: "**", redirectTo: "" }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
