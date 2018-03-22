c9a991c9b7e83654f63c3946e160815d64f7f843
Syed Tauhid Ahmed <stahmed001@gmail.com>
2018-03-22 16:30:27 +0600

    Phase 1 : Home component added to the application.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
